6f93881eee4836f94448b1256fdc93e1b7f94534
Christoph Cullmann <cullmann@kde.org>
2015-08-02 12:52:15 +0200

    less verbose categorized logging, move to ecm module later

--- src/debug.cpp
+++ src/debug.cpp
@@ -15,4 +15,9 @@
 
 #include "debug.h"
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 4, 0)
+// logging category for this framework, default: log stuff >= warning
+Q_LOGGING_CATEGORY(KJOBWIDGETS, "kf5.kjobwidgets", QtWarningMsg)
+#else
 Q_LOGGING_CATEGORY(KJOBWIDGETS, "kf5.kjobwidgets")
+#endif
