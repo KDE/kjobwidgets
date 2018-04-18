b1a7a2b253c0963fd6e5a465693e12624c0e685e
Laurent Montel <montel@kde.org>
2018-04-18 13:42:20 +0200

    warning--

--- src/kabstractwidgetjobtracker.h
+++ src/kabstractwidgetjobtracker.h
@@ -43,7 +43,7 @@ public:
      *
      * @param parent the parent of this object and of the widget displaying the job progresses
      */
-    KAbstractWidgetJobTracker(QWidget *parent = nullptr);
+    explicit KAbstractWidgetJobTracker(QWidget *parent = nullptr);
 
     /**
      * Destroys a KAbstractWidgetJobTracker
