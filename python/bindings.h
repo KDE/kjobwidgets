db4543a2cf5e53d5fc5a37aa164a9c57c48da209
Fushan Wen <qydwhotmail@gmail.com>
2025-03-28 22:38:22 +0800

    Add Python bindings

---
+++ python/bindings.h
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 Fushan Wen <qydwhotmail@gmail.com>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#pragma once
+
+#include <KAbstractWidgetJobTracker>
+#include <KDialogJobUiDelegate>
+#include <KNotificationJobUiDelegate>
+#include <KStatusBarJobTracker>
+#include <KUiServerJobTracker>
+#include <KUiServerV2JobTracker>
+#include <KWidgetJobTracker>
